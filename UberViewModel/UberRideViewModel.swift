2a53d7973e56d3a7bd2dc06fdf5505ee44a2dda8
Pramod More <pramod@biz4solutions.com>
2018-11-29 14:06:49 +0530

    access specifier changes

--- UberViewModel/UberRideViewModel.swift
+++ UberViewModel/UberRideViewModel.swift
@@ -5,14 +5,13 @@
 //  Created by Puneet Mahajan on 16/10/18.
 //  Copyright © 2018 Biz4Solutions. All rights reserved.
 //
-
 import UIKit
 import RxSwift
 import RxCocoa
 import UberRides
 import CoreLocation
 
-private protocol ViewModelType {
+public protocol ViewModelType {
     associatedtype Input
     associatedtype Output
     
@@ -20,30 +19,42 @@ private protocol ViewModelType {
     var output: Output { get }
 }
 
-class UberRideViewModel : ViewModelType {
-
-    var input: Input
-    var output: Output
+public class UberRideViewModel: ViewModelType {
+    
+    public var input: Input
+    public var output: Output
     let disposeBag = DisposeBag()
     
-    struct Input {
-        let uberButtonTap: Observable<Void>?
+    public struct Input {
+        public var uberButtonTap: Observable<Void>?
         
         // Variables for uber rides
-        var pickupLocation: CLLocationCoordinate2D?
-        var dropoffLocation: CLLocationCoordinate2D?
-        var dropoffNickname: String? = ""
-        var dropoffAddress: String? = ""
+        public var pickupLocation: CLLocationCoordinate2D?
+        public var dropoffLocation: CLLocationCoordinate2D?
+        public var dropoffNickname: String? = ""
+        public var dropoffAddress: String? = ""
+        
+        public init() {
+            
+        }
+        
+        public init(uberButtonTap: Observable<Void>, pickupLocation: CLLocationCoordinate2D?, dropoffLocation: CLLocationCoordinate2D?, dropoffNickname: String?, dropoffAddress: String?) {
+            self.uberButtonTap = uberButtonTap
+            self.pickupLocation = pickupLocation
+            self.dropoffLocation = dropoffLocation
+            self.dropoffNickname = dropoffNickname
+            self.dropoffAddress = dropoffAddress
+        }
     }
     
-    struct Output {
+    public struct Output {
         var errordata : Observable<String>
     }
     
     private let errorResultSubject = PublishSubject<String>()
     private let uberButtonTapSubject = PublishSubject<Void>()
     
-    init() {
+    public init() {
         self.input = Input.init(uberButtonTap: uberButtonTapSubject.asObserver(), pickupLocation: nil, dropoffLocation: nil, dropoffNickname: nil, dropoffAddress: nil)
         self.output = Output(errordata: errorResultSubject.asObservable())
     }
@@ -51,7 +62,7 @@ class UberRideViewModel : ViewModelType {
 
 extension UberRideViewModel {
     
-    func bookUber(input: Input) -> Output {
+    public func bookUber(input: Input) -> Output {
         
         self.input = input
         self.output = Output(errordata: errorResultSubject.asObservable())
@@ -60,7 +71,7 @@ extension UberRideViewModel {
         return output
     }
     
-    func startObservingUberButton(input: Input) {
+    public func startObservingUberButton(input: Input) {
         input.uberButtonTap?.do(onNext: { [weak self] in
             print("tap onNext")
             self?.goToUberApp()
@@ -69,7 +80,7 @@ extension UberRideViewModel {
         }).disposed(by: disposeBag)
     }
     
-    func goToUberApp() {
+    public func goToUberApp() {
         let builder = RideParametersBuilder()
         // ...
         builder.pickupLocation = CLLocation(latitude: self.input.pickupLocation?.latitude ?? 0.0, longitude: self.input.pickupLocation?.longitude ?? 0.0)
